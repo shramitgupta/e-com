315bfe3a2c33c3c5cf2c4fbd95e990d240c82c96
shramitgupta <shramitgupta1212@gmail.com>
2022-03-17 22:16:59 +0530

    trending

--- lib/stylies/home_screen_styles.dart
+++ lib/stylies/home_screen_styles.dart
@@ -11,4 +11,16 @@ class HomeScreenStylies {
     color: Colors.grey,
     fontSize: 13,
   );
+  static const TextStyle trendingProductNameStyle = TextStyle(
+    fontWeight: FontWeight.w500,
+    fontSize: 16,
+  );
+  static const TextStyle trendingProductModelStyle = TextStyle(
+    fontSize: 14,
+    color: AppColors.baseDarkPinkColor,
+  );
+  static const TextStyle trendingProductPriceStyle = TextStyle(
+    fontSize: 18,
+    color: AppColors.baseWhiteColor,
+  );
 }
