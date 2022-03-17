8f349ce2f15dcedc9c09da6cf061ced57dfc45fe
shramitgupta <shramitgupta1212@gmail.com>
2022-03-17 22:15:49 +0530

    trending

--- lib/screens/homepage/home_page.dart
+++ lib/screens/homepage/home_page.dart
@@ -107,6 +107,76 @@ class HomePage extends StatelessWidget {
     );
   }
 
+  Widget buildTrendingProduct(
+      {required String productImage,
+      required String productName,
+      required String productModel,
+      required double productPrice}) {
+    return Container(
+      margin: EdgeInsets.only(
+        top: 30,
+        left: 30,
+        right: 20,
+        bottom: 20,
+      ),
+      height: 65,
+      child: Row(
+        children: [
+          Expanded(
+            flex: 2,
+            child: Material(
+              shape: RoundedRectangleBorder(
+                  side: BorderSide.none,
+                  borderRadius: BorderRadius.circular(10.0)),
+              child: Image.network(productImage),
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    productName,
+                    style: HomeScreenStylies.trendingProductNameStyle,
+                  ),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Text(
+                    productModel,
+                    style: HomeScreenStylies.trendingProductModelStyle,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: 30.0,
+            ),
+            child: MaterialButton(
+              color: AppColors.baseLightPinkColor,
+              elevation: 0,
+              height: 45,
+              shape: RoundedRectangleBorder(
+                side: BorderSide.none,
+                borderRadius: BorderRadius.circular(0.7),
+              ),
+              onPressed: () {},
+              child: Text(
+                "\$ $productPrice",
+                style: HomeScreenStylies.trendingProductPriceStyle,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
@@ -146,6 +216,34 @@ class HomePage extends StatelessWidget {
                           productPrice: data.productPrice,
                         );
                       }),
+                ),
+                Divider(
+                  indent: 15,
+                  endIndent: 16,
+                ),
+                ShowAllWidget(
+                  leftText: "What's trending",
+                ),
+                buildTrendingProduct(
+                  productImage:
+                      'https://assets.reebok.com/images/w_600,f_auto,q_auto/cd34290e1b57479399b3aae00137ab00_9366/Classics_Mesh_Tank_Top_White_FJ3179_01_standard.jpg',
+                  productModel: 'Tank-tops',
+                  productName: 'Classics mesh tank top',
+                  productPrice: 15,
+                ),
+                buildTrendingProduct(
+                  productImage:
+                      'https://assets.reebok.com/images/w_600,f_auto,q_auto/cd34290e1b57479399b3aae00137ab00_9366/Classics_Mesh_Tank_Top_White_FJ3179_01_standard.jpg',
+                  productModel: 'Tank-tops',
+                  productName: 'Classics mesh tank top',
+                  productPrice: 15,
+                ),
+                buildTrendingProduct(
+                  productImage:
+                      'https://assets.reebok.com/images/w_600,f_auto,q_auto/cd34290e1b57479399b3aae00137ab00_9366/Classics_Mesh_Tank_Top_White_FJ3179_01_standard.jpg',
+                  productModel: 'Tank-tops',
+                  productName: 'Classics mesh tank top',
+                  productPrice: 15,
                 ),
               ],
             ),
